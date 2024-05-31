79bf2a99991afb8b4423e11826a77d0771f440e9
Vektor19 <viktor.komissarov@knu,ua>
2024-05-31 12:50:05 +0300

    created recipe and category models

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    
+    def __iter__(self):
+        return iter(self.name)
 
+    def __str__(self):
+        return self.name
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    instructions = models.TextField()
+    ingredients = models.TextField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
