a4cd2c4b66085a9543da77991072c6513f88b3c8
Vektor19 <viktor.komissarov@knu,ua>
2024-05-31 13:07:30 +0300

    Created CategoryModelTest class and setUpTestData

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
+from .models import Category,Recipe
 
 # Create your tests here.
+class CategoryModelTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        Category.objects.create(name='Appetizer')
