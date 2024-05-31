8f2e83fb60900cdbf4e66a31872f74553499aab1
Vektor19 <viktor.komissarov@knu,ua>
2024-05-31 13:10:35 +0300

    Added tests for Category

--- recipe/tests.py
+++ recipe/tests.py
@@ -6,3 +6,22 @@ class CategoryModelTest(TestCase):
     @classmethod
     def setUpTestData(cls):
         Category.objects.create(name='Appetizer')
+        
+    def test_name_label(self):
+        category = Category.objects.get(id=1)
+        field_label = category._meta.get_field('name').verbose_name
+        self.assertEquals(field_label, 'name')
+
+    def test_name_max_length(self):
+        category = Category.objects.get(id=1)
+        max_length = category._meta.get_field('name').max_length
+        self.assertEquals(max_length, 255)
+
+    def test_iter_method(self):
+        category = Category.objects.get(id=1)
+        self.assertEquals(list(iter(category)), list(iter('Appetizer')))
+
+
+    def test_str_method(self):
+        category = Category.objects.get(id=1)
+        self.assertEquals(str(category), 'Appetizer')
